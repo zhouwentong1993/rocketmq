efd21220c338c61e11802b082dc8fe15e0f52f8f
zhouwentong <zhouwentong1993@gmail.com>
2021-11-24 10:14:19 +0800

    调整脚本 & 文件格式

--- test/src/main/java/com/wentong/rocketmq/test/SampleProducer.java
+++ test/src/main/java/com/wentong/rocketmq/test/SampleProducer.java
@@ -12,12 +12,12 @@ import java.nio.charset.StandardCharsets;
 public class SampleProducer {
 
     public static void main(String[] args) throws Exception {
-        DefaultMQProducer producer = new DefaultMQProducer("sync-group-name");
+        DefaultMQProducer producer = new DefaultMQProducer("syn1c-group-name");
         producer.setNamesrvAddr("localhost:9876");
         producer.start();
 
         Message message = new Message("TopicTest", "TagA", ("Hello World").getBytes(StandardCharsets.UTF_8));
-        SendResult result = producer.send(message, 100_0000);
+        SendResult result = producer.send(message);
         System.out.println(result);
 
 //        for (int i = 0; i < 100; i++) {
